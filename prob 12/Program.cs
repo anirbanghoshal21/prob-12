d4bd8cfcf4980a2b0f37bd3c9553c483a5376a69
username <username@cognizant.com>
2022-08-22 21:17:10 +0530

    Add project files.

---
+++ prob 12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 30, 40, 100, 170 };
+            int max = array[0];
+            int min = array[0];
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine(max);
+            Console.WriteLine(min);
+            Console.ReadKey();
+        }
+    }
+}
